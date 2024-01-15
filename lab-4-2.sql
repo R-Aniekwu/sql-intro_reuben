ccc27ada3c892761620d7de1f0f9f0dd9517fcaa
Reuben Tochukwu Aniekwu <reuben.aniekwu@kellogg.northwestern.edu>
2024-01-15 08:55:18 +0000

    Completed practice assignments

--- lab-4-2.sql
+++ lab-4-2.sql
@@ -5,6 +5,12 @@
 -- WHERE column1 = expression1
 -- AND column2 = expression2
 
+SELECT players.first_name, players.last_name
+FROM ((players INNER JOIN stats ON stats.player_id = players.id)
+INNER JOIN teams on teams.id = stats.team_id)
+WHERE teams.year = "2020" AND teams.name = "Chicago Cubs";
+
+
 -- Expected result: 47 rows starting with
 --
 -- +------------+-----------+
